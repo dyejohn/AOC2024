0839bd74afa2b17635cd5739e776db441ef1ba4f
John Dye <john@geekydevdyes.com>
2024-12-02 00:40:57 -0500

    day 2 part 1 solved

---
+++ AOC2024Day2/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Numerics;
+
+using (var reader = new StreamReader("../../../../data/AOC2024Day2/input.txt"))
+{
+    var validReportCount = 0;
+    while(!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        var elements = line.Split(" ").Select(x => int.Parse(x.Trim())).ToList();
+
+        // trend will match on the first element every time but that's fine.
+        var differential = elements[1] - elements[0];
+        bool validReport = true;
+
+        var trend = 0;
+        if (differential == 0)
+        {
+            validReport = false;
+        }
+        else
+        {
+            trend = (differential) / Math.Abs(differential);
+        }
+       
+        for(int i = 1; i < elements.Count; i++)
+        {
+            differential = elements[i] - elements[i - 1];
+            if (differential == 0 || Math.Abs(differential) > 3)
+            {
+                validReport = false;
+                break;
+            }
+            var newTrend = (differential) / Math.Abs(differential);
+            if (newTrend != trend)
+            {
+                validReport = false;
+                break;
+            }
+        }
+        if (validReport)
+        {
+            validReportCount++;
+        }
+
+    }
+
+    Console.WriteLine(validReportCount);
+
+}
